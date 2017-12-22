1ca6ce4742fea1032c87a5d63161caf2838c820c
satoshi <swatanabe@jp.sony.com>
2017-12-22 11:48:11 +0900

    fix INPUT_FILES env parse logic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,7 @@ set -eo pipefail
 GOOGLE_PROJECT_ID="ace-ripsaw-671"
 BIGQUERY_DATASET="watanabe_dataset"
 DATASTORE_NAMESPACE="watanabe"
-declare -a INPUT_FILES=(
-    "gs://classification-data/*/*.gz",
-    "gs://classification-data-2/*/*.gz",
-    "gs://classification-data-4/*/*.txt",
-    "gs://classification-data-data-on-demand/*/*.gz"
-)
+INPUT_FILES="gs://classification-data/*/*.gz,gs://classification-data-2/*/*.gz,gs://classification-data-4/*/*.txt,gs://classification-data-data-on-demand/*/*.gz"
 
 # It is important that the following directory is added as a data volume.
 HASH_PATH="/var/opt/cd-pipeline/input-files-hash"
@@ -115,9 +110,11 @@ run_classifier_data_transfer_pipeline() {
         bq rm -f $segment_table
         bq mk --schema user_id:string,class:string -t $segment_table
 
-        for segment_file in ${INPUT_FILES[@]}; do
+        local files_array=${INPUT_FILES//,/ }
+        for segment_file in ${files_array[@]}; do
             bq load $segment_table $segment_file
         done
+
 
         #
         # Step 2: Create new classification table
@@ -126,6 +123,7 @@ run_classifier_data_transfer_pipeline() {
 
         bq query --use-legacy-sql=False --distination-table=$new_classification_table \
         "SELECT user_id, ARRAY_AGG(class) AS classes FROM `$segment_table` GROUP BY user_id"
+
 
         #
         # Step 3: Create a diff table for new_classification and last_classification
@@ -164,6 +162,7 @@ run_classifier_data_transfer_pipeline() {
             "
         fi
 
+
         #
         # Step 4: Start the DataFlow operation to synchronize BigQuery table and DataStore
         #
@@ -181,12 +180,14 @@ run_classifier_data_transfer_pipeline() {
 
         echo "$new_hash" > $HASH_PATH
 
+
         #
         # Step 6: Update current classification table name
         #
         echo "Step 6: Update current classification table name"
 
         ds_util.py set --key=$DATASTORE_LAST_TABLE_KEY --namespace=$DATASTORE_NAMESPACE --kind=$DATASTORE_KIND --prop=$DATASTORE_LAST_TABLE_PROP --value=$new_classification_table
+
 
         #
         # Step 7: delete unnecessary BigQuery tables
@@ -217,7 +218,7 @@ case "$1" in
             -v /var/opt/ncd-pipeline:/var/opt/ncd-pipeline \\
             -e GOOGLE_PROJECT_ID=\"NameOfProject\" \\
             -e DATAFLOW_TEMP_LOCATION=gs://bucketName \\
-            -e INPUT_FILES=gs://bucketName/inputFolder \\
+            -e INPUT_FILES=gs://bucketName/inputFolder/*.txt,gs://bucketName/inputFolder2/*.gz \\
             -e BIGQUERY_DATASET=bq_dataset \\
             -e DATASTORE_NAMESPACE=namespace \\
             ncd-pipeline run_classifier_data_transfer_pipeline"
